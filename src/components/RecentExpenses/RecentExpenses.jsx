673f714a642143c5c86ed49c3e3fed3deb780b32
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-29 12:46:31 +0530

    Implementing pagination logic along with its styling

--- src/components/RecentExpenses/RecentExpenses.jsx
+++ src/components/RecentExpenses/RecentExpenses.jsx
@@ -5,6 +5,7 @@ import foodIcon from "../../assets/food-icon.svg";
 import entertainmentIcon from "../../assets/entertainment-icon.svg";
 import travelIcon from "../../assets/travel-icon.svg";
 import deleteIcon from "../../assets/delete-icon.svg";
+import { useState } from "react";
 
 const getCategoryIcon = (category) => {
   const lowercaseCategory = category.toLowerCase();
@@ -27,25 +28,60 @@ const RecentExpenses = () => {
     console.log(event.target);
   };
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const historyPerPage = 3;
+
+  const lastItem = currentPage * historyPerPage;
+  const firstItem = lastItem - historyPerPage;
+  const currentItemList = expenseList.slice(firstItem, lastItem);
+
+  const nextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+  const prevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
   return (
     <div className={styles.container}>
       <h2>Recent Expenses</h2>
       <div className={styles.listContainer}>
-        {expenseList.length > 0 ? (
-          expenseList.map((expense, index) => (
-            <div key={index} className={styles.card}>
-              <div className={styles.expenseDetails}>
-                {getCategoryIcon(expense.Category)}
-                <h3>{expense.Title}</h3>
+        {currentItemList.length > 0 ? (
+          <>
+            {currentItemList.map((expense, index) => (
+              <div key={index} className={styles.card}>
+                <div className={styles.expenseDetails}>
+                  {getCategoryIcon(expense.Category)}
+                  <h3>{expense.Title}</h3>
+                </div>
+                <div className={styles.pricingDetails}>
+                  <p style={{ color: "#F4BB4A" }}>&#8377;{expense.Price}</p>
+                  <button className={styles.btn} onClick={handleDelete}>
+                    <img src={deleteIcon} alt="delete_icon" />
+                  </button>
+                </div>
               </div>
-              <div className={styles.pricingDetails}>
-                <p style={{ color: "#F4BB4A" }}>&#8377;{expense.Price}</p>
-                <button className={styles.btn}>
-                  <img src={deleteIcon} alt="delete_icon" />;
-                </button>
-              </div>
+            ))}
+            <div className={styles.pagination}>
+              <button
+                className={styles.btns}
+                onClick={prevPage}
+                disabled={currentPage === 1}
+              >
+                &#8592;
+              </button>
+              <span className={styles.currentPage}>{currentPage}</span>
+              <button
+                className={styles.btns}
+                onClick={nextPage}
+                disabled={
+                  Math.ceil(expenseList.length / historyPerPage) <= currentPage
+                }
+              >
+                &#8594;
+              </button>
             </div>
-          ))
+          </>
         ) : (
           <div className={styles.emptyHistory}>
             <img src={historyIcon} alt="history_icon" />
